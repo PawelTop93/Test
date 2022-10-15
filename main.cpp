874e6b9a6dff3d346c4f2f1305d542b2612affa2
Pawel <topolski.pawel93@gmail.com>
2022-10-15 23:46:56 +0200

    Dodanie trzeciej linii

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main()
 {
     std::cout<<"NIC A DDDAAADDSS"<<std::endl;
     std::cout<<"CosTAMCiekawegoLubNie"<<std::endl;
+    std::cout<<" DOdanie jeszcze kolejnej linii"<<std::endl;
     return 0;
 }
